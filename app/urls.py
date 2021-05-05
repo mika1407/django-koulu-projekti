96b0238227b6282fa401d2e874cbc31bdf6cf027
mika1407 <koneautomaatio@hotmail.com>
2021-05-05 22:31:25 +0300

    navogointi buttonit lisätty

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import landingview
+from .views import landingview, supplierlistview, productlistview
 
 urlpatterns = [
     path('', landingview),
+    path('suppliers/', supplierlistview),
+    path('products/', productlistview),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Supplier, Product
 
 # Create your views here.
 def landingview(request):
-  return render (request, "landingpage.html")
+    return render (request, "landingpage.html")
+
+def supplierlistview(request):
+    supplierlist = Supplier.objects.all()
+    context = {'suppliers': supplierlist}
+    return render (request, "suppliers.html", context)
+
+def productlistview(request):
+    productlist = Product.objects.all()
+    context = {'products': productlist}
+    return render (request, "products.html", context)
