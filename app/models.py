0b7bf4a103136d028b52d05b9ec638c22c9f7eff
mika1407 <koneautomaatio@hotmail.com>
2021-05-20 14:48:37 +0300

    datanlistaus

--- app/models.py
+++ app/models.py
@@ -9,9 +9,13 @@ class Supplier(models.Model):
     email = models.CharField(max_length=50, default= 'timo.tilli@tilli.com')
     country = models.CharField(max_length=20, default= 'Finland')
 
+    # class Meta:
+    #     ordering =['companyname']
+
 class Product(models.Model):
     productname = models.CharField(max_length = 20, default = "laku")
     packagesize = models.CharField(max_length = 20, default = 3)
     unitprice = models.DecimalField(max_digits=10, decimal_places=2, default=1.00)
     unitsinstock = models.IntegerField(default= 3)
     companyname = models.CharField(max_length= 50, default= 'lakufirma')
+    # supplier = models.ForeignKey(Supplier, on_)
