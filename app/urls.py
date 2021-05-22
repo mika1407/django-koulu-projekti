37e2ed5ae42ff2492f4b4f27e278b04a66283566
mika1407 <koneautomaatio@hotmail.com>
2021-05-22 06:30:27 +0300

    poisto ja lisäys toimii

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import landingview, supplierlistview, productlistview
+from .views import landingview, supplierlistview, productlistview, addsupplier, addproduct, deletesupplier, deleteproduct
 
 urlpatterns = [
     path('', landingview),
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
+    path('delete-supplier/<int:iidee>/',deletesupplier),
     path('products/', productlistview),
+    path('add-product/', addproduct),
+    path('delete-product/<int:iidee>/',deleteproduct),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Supplier, Product
 
 # Create your views here.
@@ -10,7 +10,36 @@ def supplierlistview(request):
     context = {'suppliers': supplierlist}
     return render (request, "suppliers.html", context)
 
+def addsupplier(request):
+    a = request.POST['companyname']
+    b = request.POST['contactname']
+    c = request.POST['address']
+    d = request.POST['phone']
+    e = request.POST['email']
+    f = request.POST['country']
+    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+def deletesupplier(request,iidee):
+    Supplier.objects.filter(id = iidee).delete()
+    return redirect(request.META['HTTP_REFERER'])
+
+# PRODUCTS HANDLING AND VIEW FUNCTIONS
+
 def productlistview(request):
     productlist = Product.objects.all()
     context = {'products': productlist}
     return render (request, "products.html", context)
+
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['packagesize']
+    c = request.POST['unitprice']
+    d = request.POST['unitsinstock']
+    e = request.POST['companyname']
+    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, companyname = e).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+def deleteproduct(request,iidee):
+    Product.objects.filter(id = iidee).delete()
+    return redirect(request.META['HTTP_REFERER'])
